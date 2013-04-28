1f7ca1ec3869db8f0acaaa1fb9be5ad790d75f76
Taylor <trflagg@gmail.com>
2013-04-28 14:20:45 -0400

    Stops only moving animation.

--- js/Mover.js
+++ js/Mover.js
@@ -5,6 +5,7 @@ define(['lib/underscore'], function() {
             this._moving = false;
             this._movementType = null;
             this._angle = null;
+            this._currentAnimation = null;
         },
         startMoving: function() {
             this._moving = true;
@@ -61,7 +62,41 @@ define(['lib/underscore'], function() {
             animation[yPos] = this.sprite.attr(yPos) + dy;
 
             //TODO: Only stop the last moving animation, not all of them!
-            this.sprite.stop().animate(animation, 20000);
+            if (this._currentAnimation) {
+                this.sprite.stop(this._currentAnimation);
+            }
+
+            this._currentAnimation = Raphael.animation(animation, 20000);
+            this.sprite.animate(this._currentAnimation);
+        },
+
+
+        drawPath: function( canvas, pathstr, duration, attr, callback )
+        {
+            var guide_path = canvas.path( pathstr ).attr( { stroke: "none", fill: "none" } );
+            var path = canvas.path( guide_path.getSubpath( 0, 1 ) ).attr( attr );
+            var total_length = guide_path.getTotalLength( guide_path );
+            var last_point = guide_path.getPointAtLength( 0 );
+            var start_time = new Date().getTime();
+            var interval_length = 50;
+            var result = path;        
+
+            var interval_id = setInterval( function()
+            {
+                var elapsed_time = new Date().getTime() - start_time;
+                var this_length = elapsed_time / duration * total_length;
+                var subpathstr = guide_path.getSubpath( 0, this_length );            
+                attr.path = subpathstr;
+
+                path.animate( attr, interval_length );
+                if ( elapsed_time >= duration )
+                {
+                    clearInterval( interval_id );
+                    if ( callback != undefined ) callback();
+                        guide_path.remove();
+                }                                       
+            }, interval_length );  
+            return result;
         }
     }
 })
